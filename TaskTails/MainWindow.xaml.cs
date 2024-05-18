057ec3692a0dd70eb9ab69ecd033d4f36ec8feef
OBSIDIAN\lwarren <developer@warrencode.com>
2024-05-18 12:24:56 -0600

    initial rough logic for user egg generation

--- TaskTails/MainWindow.xaml.cs
+++ TaskTails/MainWindow.xaml.cs
@@ -20,9 +20,48 @@ namespace TaskTails
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string eggPath1;
+        public string eggPath2;
+        public string eggPath3;
+
         public MainWindow()
         {
             InitializeComponent();
         }
-    }
-}
+
+        private string generateRandomEgg() {
+            // 14 total eggs
+            Random random = new Random();
+            int num = random.Next(0, 13);
+            
+            // return based on random number generated 
+            switch (num)
+            {
+                case 0: return "";
+                case 1: return "";
+                case 2: return "";
+                case 3: return "";
+                case 4: return "";
+                case 5: return "";
+                case 6: return "";
+                case 7: return "";
+                case 8: return "";
+                case 9: return "";
+                case 10: return "";
+                case 11: return "";
+                case 12: return "";
+                case 13: return "";
+            }
+        
+        
+        }
+
+
+
+
+    }// end main window
+
+
+
+
+}// end namespace
